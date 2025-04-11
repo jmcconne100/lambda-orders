0b9d73744029041368d8485a1e84482ce13c5425
Jon McConnell <87622525+jmcconne100@users.noreply.github.com>
2025-04-10 23:27:06 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -16,7 +16,7 @@ def handler(event, context):
     for i in range(1, 2000):
         customer_id = random.randint(1, 100_000)
         product_id = random.randint(1, 1000)
-        signup_date = faker.date_between(start_date=date(year, 1, 1), end_date=date(year, 12, 31))
+        order_date = faker.date_between(start_date=date(year, 1, 1), end_date=date(year, 12, 31))
         quantity = random.randint(1, 10)
         unit_price = round(random.uniform(5.0, 1000.0), 2)
         total_price = round(unit_price * quantity, 2)
